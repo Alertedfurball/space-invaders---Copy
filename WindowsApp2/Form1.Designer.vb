4da5b731f721d712cfc5c6616f0d936b07a1612c
Buhr-Arndt4403097 <4403097@sps-student.sk.ca>
2021-11-04 11:56:52 -0600

    Add project files.

---
+++ WindowsApp2/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.allen1 = New System.Windows.Forms.PictureBox()
+        Me.shipp = New System.Windows.Forms.PictureBox()
+        Me.AllenTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.allen2 = New System.Windows.Forms.PictureBox()
+        Me.allen3 = New System.Windows.Forms.PictureBox()
+        Me.allen4 = New System.Windows.Forms.PictureBox()
+        Me.allen5 = New System.Windows.Forms.PictureBox()
+        Me.allen6 = New System.Windows.Forms.PictureBox()
+        CType(Me.allen1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.shipp, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.allen2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.allen3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.allen4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.allen5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.allen6, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'allen1
+        '
+        Me.allen1.Image = CType(resources.GetObject("allen1.Image"), System.Drawing.Image)
+        Me.allen1.Location = New System.Drawing.Point(197, 12)
+        Me.allen1.Name = "allen1"
+        Me.allen1.Size = New System.Drawing.Size(65, 60)
+        Me.allen1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.allen1.TabIndex = 0
+        Me.allen1.TabStop = False
+        Me.allen1.Tag = "left"
+        '
+        'shipp
+        '
+        Me.shipp.BackgroundImage = Global.WindowsApp2.My.Resources.Resources._501_5011901_space_invaders_ship_transparent_hd_png_download
+        Me.shipp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.shipp.Location = New System.Drawing.Point(361, 355)
+        Me.shipp.Name = "shipp"
+        Me.shipp.Size = New System.Drawing.Size(94, 83)
+        Me.shipp.TabIndex = 1
+        Me.shipp.TabStop = False
+        '
+        'AllenTimer
+        '
+        Me.AllenTimer.Enabled = True
+        '
+        'allen2
+        '
+        Me.allen2.Image = CType(resources.GetObject("allen2.Image"), System.Drawing.Image)
+        Me.allen2.Location = New System.Drawing.Point(268, 12)
+        Me.allen2.Name = "allen2"
+        Me.allen2.Size = New System.Drawing.Size(65, 60)
+        Me.allen2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.allen2.TabIndex = 2
+        Me.allen2.TabStop = False
+        Me.allen2.Tag = "left"
+        '
+        'allen3
+        '
+        Me.allen3.Image = CType(resources.GetObject("allen3.Image"), System.Drawing.Image)
+        Me.allen3.Location = New System.Drawing.Point(339, 12)
+        Me.allen3.Name = "allen3"
+        Me.allen3.Size = New System.Drawing.Size(65, 60)
+        Me.allen3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.allen3.TabIndex = 3
+        Me.allen3.TabStop = False
+        Me.allen3.Tag = "left"
+        '
+        'allen4
+        '
+        Me.allen4.Image = CType(resources.GetObject("allen4.Image"), System.Drawing.Image)
+        Me.allen4.Location = New System.Drawing.Point(410, 12)
+        Me.allen4.Name = "allen4"
+        Me.allen4.Size = New System.Drawing.Size(65, 60)
+        Me.allen4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.allen4.TabIndex = 4
+        Me.allen4.TabStop = False
+        Me.allen4.Tag = "left"
+        '
+        'allen5
+        '
+        Me.allen5.Image = CType(resources.GetObject("allen5.Image"), System.Drawing.Image)
+        Me.allen5.Location = New System.Drawing.Point(481, 12)
+        Me.allen5.Name = "allen5"
+        Me.allen5.Size = New System.Drawing.Size(65, 60)
+        Me.allen5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.allen5.TabIndex = 5
+        Me.allen5.TabStop = False
+        Me.allen5.Tag = "left"
+        '
+        'allen6
+        '
+        Me.allen6.Image = CType(resources.GetObject("allen6.Image"), System.Drawing.Image)
+        Me.allen6.Location = New System.Drawing.Point(552, 12)
+        Me.allen6.Name = "allen6"
+        Me.allen6.Size = New System.Drawing.Size(65, 60)
+        Me.allen6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.allen6.TabIndex = 6
+        Me.allen6.TabStop = False
+        Me.allen6.Tag = "left"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.allen6)
+        Me.Controls.Add(Me.allen5)
+        Me.Controls.Add(Me.allen4)
+        Me.Controls.Add(Me.allen3)
+        Me.Controls.Add(Me.allen2)
+        Me.Controls.Add(Me.shipp)
+        Me.Controls.Add(Me.allen1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.allen1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.shipp, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.allen2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.allen3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.allen4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.allen5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.allen6, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents allen1 As PictureBox
+    Friend WithEvents shipp As PictureBox
+    Friend WithEvents AllenTimer As Timer
+    Friend WithEvents allen2 As PictureBox
+    Friend WithEvents allen3 As PictureBox
+    Friend WithEvents allen4 As PictureBox
+    Friend WithEvents allen5 As PictureBox
+    Friend WithEvents allen6 As PictureBox
+End Class
---
+++ WindowsApp2/Form1.vb
@@ -0,0 +1,173 @@
+﻿Public Class Form1
+    Dim shipploc, allenloc As Point
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+
+
+        'move left
+        If (e.KeyCode = Keys.A Or e.KeyCode = Keys.Left) And shipp.Location.X >= 5 Then
+            shipploc = New Point(shipp.Location.X - 5, shipp.Location.Y)
+            shipp.Location = shipploc
+        End If
+        'move right
+        If (e.KeyCode = Keys.D Or e.KeyCode = Keys.Right) And shipp.Location.X <= Me.Width - shipp.Width - 22 Then
+            shipploc = New Point(shipp.Location.X + 5, shipp.Location.Y)
+            shipp.Location = shipploc
+        End If
+    End Sub
+
+    Private Sub AllenTimer_Tick(sender As Object, e As EventArgs) Handles AllenTimer.Tick
+        'move allen left
+        If allen1.Tag = "left" Then
+
+            allenloc = New Point(allen1.Location.X - 5, allen1.Location.Y)
+            allen1.Location = allenloc
+            If allen1.Left <= 5 Then 'if hit left wall
+
+
+
+                allenloc = New Point(allen1.Location.X + 5, allen1.Location.Y + 20)
+                allen1.Location = allenloc
+                allen1.Tag = "right"
+            End If
+
+            'move allen right
+        ElseIf allen1.Tag = "right" Then
+            allen2.Tag = "right"
+            allen3.Tag = "right"
+            allen4.Tag = "right"
+            allen5.Tag = "right"
+            allen6.Tag = "right"
+
+
+            allenloc = New Point(allen1.Location.X + 5, allen1.Location.Y)
+            allen1.Location = allenloc
+            If allen1.Right >= Me.Width - 400 Then 'if hit right wall
+                allenloc = New Point(allen1.Location.X - 5, allen1.Location.Y + 20)
+
+                allen1.Location = allenloc
+                allen1.Tag = "left"
+                If allen1.Tag = "left" Then
+                    allen2.Tag = "left"
+                    allen3.Tag = "left"
+                    allen4.Tag = "left"
+                    allen5.Tag = "left"
+                    allen6.Tag = "left"
+
+                End If
+            End If
+        End If
+        'move allen left
+        If allen2.Tag = "left" Then
+            allenloc = New Point(allen2.Location.X - 5, allen2.Location.Y)
+            allen2.Location = allenloc
+            If allen1.Left <= 5 Then 'if hit left wall
+
+                allenloc = New Point(allen2.Location.X + 5, allen2.Location.Y + 20)
+                allen2.Location = allenloc
+
+            End If
+
+            'move allen right
+        ElseIf allen2.Tag = "right" Then
+            allenloc = New Point(allen2.Location.X + 5, allen2.Location.Y)
+            allen2.Location = allenloc
+            If allen1.Right >= Me.Width - 50 Then 'if hit right wall
+                allenloc = New Point(allen2.Location.X - 5, allen2.Location.Y + 20)
+
+                allen2.Location = allenloc
+
+            End If
+        End If
+        'move allen left
+        If allen3.Tag = "left" Then
+            allenloc = New Point(allen3.Location.X - 5, allen3.Location.Y)
+            allen3.Location = allenloc
+            If allen1.Left <= 5 Then 'if hit left wall
+
+                allenloc = New Point(allen3.Location.X + 5, allen3.Location.Y + 20)
+                allen3.Location = allenloc
+
+            End If
+
+            'move allen right
+        ElseIf allen3.Tag = "right" Then
+            allenloc = New Point(allen3.Location.X + 5, allen3.Location.Y)
+            allen3.Location = allenloc
+            If allen1.Right >= Me.Width - 50 Then 'if hit right wall
+                allenloc = New Point(allen3.Location.X - 5, allen3.Location.Y + 20)
+
+                allen3.Location = allenloc
+
+            End If
+        End If
+        'move allen left
+        If allen4.Tag = "left" Then
+            allenloc = New Point(allen4.Location.X - 5, allen4.Location.Y)
+            allen4.Location = allenloc
+            If allen1.Left <= 5 Then 'if hit left wall
+
+                allenloc = New Point(allen4.Location.X + 5, allen4.Location.Y + 20)
+                allen4.Location = allenloc
+
+            End If
+
+            'move allen right
+        ElseIf allen4.Tag = "right" Then
+            allenloc = New Point(allen4.Location.X + 5, allen4.Location.Y)
+            allen4.Location = allenloc
+            If allen1.Right >= Me.Width - 50 Then 'if hit right wall
+                allenloc = New Point(allen4.Location.X - 5, allen4.Location.Y + 20)
+
+                allen4.Location = allenloc
+
+            End If
+        End If
+        'move allen left
+        If allen5.Tag = "left" Then
+            allenloc = New Point(allen5.Location.X - 5, allen5.Location.Y)
+            allen5.Location = allenloc
+            If allen1.Left <= 5 Then 'if hit left wall
+
+                allenloc = New Point(allen5.Location.X + 5, allen5.Location.Y + 20)
+                allen5.Location = allenloc
+
+            End If
+
+            'move allen right
+        ElseIf allen5.Tag = "right" Then
+            allenloc = New Point(allen5.Location.X + 5, allen5.Location.Y)
+            allen5.Location = allenloc
+            If allen1.Right >= Me.Width - 50 Then 'if hit right wall
+                allenloc = New Point(allen5.Location.X - 5, allen5.Location.Y + 20)
+
+                allen5.Location = allenloc
+
+            End If
+        End If
+
+        Dim point As Point = New Point(allen6.Location.X - 5, allen6.Location.Y)
+        'move allen left
+        If allen6.Tag = "left" Then
+            allenloc = New Point(allen6.Location.X - 5, allen6.Location.Y)
+            allen6.Location = allenloc
+            If allen6.Left <= 5 Then 'if hit left wall
+
+                allenloc = New Point(allen6.Location.X + 5, allen6.Location.Y + 20)
+                allen6.Location = allenloc
+                allen6.Tag = "right"
+            End If
+
+            'move allen right
+        ElseIf allen6.Tag = "right" Then
+            allenloc = New Point(allen6.Location.X + 5, allen6.Location.Y)
+            allen6.Location = allenloc
+            If allen6.Right >= Me.Width Then 'if hit right wall
+                allenloc = point
+
+                allen6.Location = allenloc
+                allen6.Tag = "left"
+            End If
+        End If
+    End Sub
+End Class
